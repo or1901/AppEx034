dbd6d872489fea832b18b3fa1fb15cfada1afddd
orira22 <oriroitzaid@gmail.com>
2022-12-22 16:37:16 +0200

    Final commit - a button that presents how many clicks have made on it, according to the game 7 BOOM

--- app/src/main/java/com/example/appex034/MainActivity.java
+++ app/src/main/java/com/example/appex034/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.appex034;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn;
+    int clicks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.btn);
+        clicks = 0;
+    }
+
+    public void sevenBoom(View view) {
+        clicks++;
+
+        if (clicks % 7 == 0)
+            btn.setText("BOOM!");
+        else
+            btn.setText("This is a click number: " + clicks);
     }
 }
